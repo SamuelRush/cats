6082e5b809945eec09efa4434a29071ea15aac40
Chris Mastel <chris.mastel@gmail.com>
2019-10-22 22:03:20 +0000

    fixed the error loop

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -5,8 +5,13 @@ const breedDetailsFromFile = function(breed, callback) {
   console.log('breedDetailsFromFile: Calling readFile...');
   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
     // ISSUE: Returning from inner callback function, not our main function.
-    console.log('Callback: I have the data!')
-    if (!error) callback(data); 
+    if (!error) {
+      console.log('Callback: I have the data!');
+      callback(data)
+    } else {
+      console.log('data not found');
+    }
+    
   });
   // ISSUE: Attempting to return data out here will also not work.
   //        Currently not returning anything from here, so this function returns undefined.
